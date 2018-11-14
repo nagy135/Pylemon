75dde228bb86ff70db35c7493ddd27fbe73ad03a
nagy135 <viktor.nagy1995@gmail.com>
2018-11-14 14:32:45 +0100

    some work

--- pylemon.py
+++ pylemon.py
@@ -1,31 +1,69 @@
 import signal
+import subprocess
 
 class Pylemon(object):
     def __init__(self):
-        self.data = list()
+        self.outputs = dict()
+        self.outputs['left'] = dict()
+        self.outputs['right'] = dict()
+        self.outputs['center'] = dict()
+
         self.states = {
                 'date': False,
                 'layout': False
         }
+
+        self.functions = {
+                'date': self.get_date,
+                'layout': self.get_layout
+        }
+
+        self.positions = {
+                'date': 'left',
+                'layout': 'right'
+        }
+
+        subprocess.Popen(['/home/infiniter/Code/Pylemon/pylemon_wakeup', '2'])
+        self.run()
+
     def get_date(self):
         return 'mydate'
     def get_layout(self):
         return 'mylayout'
-    def refresh(self):
+
+    def refresh_user(self):
         try:
             with open('/tmp/refresh', 'r') as t:
                 target = t.read().replace('\n','')
             with open('/tmp/refresh', 'w') as t:
                 t.write('')
         except FileNotFoundError:
+            with open('/tmp/refresh', 'w') as t:
+                t.write('')
             return
         if target == 'date':
             self.states['date'] = False
+        if target == 'layout':
+            self.states['layout'] = False
+        self.refresh()
+
+    def refresh(self):
         for key in self.states:
-            if key is True
+            if self.states[key] is False:
+                self.outputs[self.positions[key]][key] = self.functions[key]()
+                self.states[key] = True
+        left = '%{l}' + ''.join(self.outputs['left'])
+        center = '%{c}' + ''.join(self.outputs['center'])
+        right = '%{r}' + ''.join(self.outputs['right'])
+        subprocess.Popen(['echo', left, center, right], shell=True)
+
+
+
     def run(self):
-        signal.signal(signal.SIGUSR1, refresh)
+        signal.signal(signal.SIGSEGV, self.refresh)
+        signal.signal(signal.SIGUSR1, self.refresh_user)
+        # initial pain
+        self.refresh()
         while True:
             signal.pause()
 instance = Pylemon()
-instance.run()
